7dbfe30d52fbd8c2b77c407e6f2ee434a3c96c6c
Akshay Bartakke <Akshay.Bartakke@kantar.com>
2025-07-07 10:24:50 +0530

    server file is added

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// const productsRoutes = require('./routes/products');
+// const cartRoutes = require('./routes/cart');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+// app.use('/products', productsRoutes);
+// app.use('/cart', cartRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
